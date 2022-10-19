601525b727cebfc9ebae1a344e465046c276325a
pmutundu <mutundu.nguma@gmail.com>
2022-10-19 18:54:47 +0300

    write basic calculator function

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+var add = function (number1, number2) {
+    return number1 + number2;
+};
+var number1 = parseInt(prompt("enter a number"));
+var number2 = parseInt(prompt("enter another number"));
+alert(add(number1, number2));
+
+
+var subtract = function (number1, number2) {
+    return number1 - number2;
+};
+alert(subtract(10, 5));
